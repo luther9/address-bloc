5416309271f0e0d2288416a5ea7e5da6e7561b87
Luther <lutheroto@gmail.com>
2016-04-20 16:01:21 -0400

    Use 'before :each' clauses for repeated statements.

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -24,14 +24,15 @@ RSpec.describe AddressBook do
   end
 
   describe '#add_entry' do
-    it 'adds only one entry to the address book' do
+    before :each do
       book.add_entry 'Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com'
+    end
 
+    it 'adds only one entry to the address book' do
       expect(book.entries.size).to eq 1
     end
 
     it 'adds the correct information to entries' do
-      book.add_entry 'Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com'
       new_entry = book.entries[0]
 
       expect(new_entry.name).to eq 'Ada Lovelace'
@@ -41,88 +42,82 @@ RSpec.describe AddressBook do
   end
 
   describe '#import_from_csv' do
-    it 'imports the correct number of entries' do
+    before :each do
       book.import_from_csv 'entries.csv'
-      book_size = book.entries.size
+    end
 
+    it 'imports the correct number of entries' do
+      book_size = book.entries.size
       expect(book_size).to eq 5
     end
 
     it 'imports the 1st entry' do
-      book.import_from_csv 'entries.csv'
       entry_one = book.entries[0]
       check_entry entry_one, "Bill", "555-555-4854", "bill@blocmail.com"
     end
 
     it 'imports the 2nd entry' do
-      book.import_from_csv 'entries.csv'
       entry_two = book.entries[1]
       check_entry entry_two, "Bob", "555-555-5415", "bob@blocmail.com"
     end
 
     it 'imports the 3rd entry' do
-      book.import_from_csv 'entries.csv'
       entry_three = book.entries[2]
       check_entry entry_three, "Joe", "555-555-3660", "joe@blocmail.com"
     end
 
     it 'imports the 4th entry' do
-      book.import_from_csv 'entries.csv'
       entry_four = book.entries[3]
       check_entry entry_four, "Sally", "555-555-4646", "sally@blocmail.com"
     end
 
     it 'imports the 5th entry' do
-      book.import_from_csv 'entries.csv'
       entry_five = book.entries[4]
       check_entry entry_five, "Sussie", "555-555-2036", "sussie@blocmail.com"
     end
   end
 
   describe '#binary_search' do
-    it 'searches AddressBook for a non-existent entry' do
+    before :each do
       book.import_from_csv 'entries.csv'
+    end
+
+    it 'searches AddressBook for a non-existent entry' do
       entry = book.binary_search 'Dan'
       expect(entry).to be_nil
     end
 
     it 'searches AddressBook for Bill' do
-      book.import_from_csv 'entries.csv'
       entry = book.binary_search 'Bill'
       expect(entry).to be_a Entry
       check_entry entry, "Bill", "555-555-4854", "bill@blocmail.com"
     end
 
     it 'searches AddressBook for Bob' do
-      book.import_from_csv 'entries.csv'
       entry = book.binary_search 'Bob'
       expect(entry).to be_a Entry
       check_entry entry, "Bob", "555-555-5415", "bob@blocmail.com"
     end
 
     it 'searches AddressBook for Joe' do
-      book.import_from_csv 'entries.csv'
       entry = book.binary_search 'Joe'
       expect(entry).to be_a Entry
       check_entry entry, "Joe", "555-555-3660", "joe@blocmail.com"
     end
 
     it 'searches AddressBook for Sally' do
-      book.import_from_csv 'entries.csv'
       entry = book.binary_search 'Sally'
       expect(entry).to be_a Entry
       check_entry entry, "Sally", "555-555-4646", "sally@blocmail.com"
     end
 
     it 'searches AddressBook for Sussie' do
-      book.import_from_csv 'entries.csv'
       entry = book.binary_search 'Sussie'
       expect(entry).to be_a Entry
       check_entry entry, "Sussie", "555-555-2036", "sussie@blocmail.com"
     end
 
     it 'searches AddressBook for Billy' do
-      book.import_from_csv 'entries.csv'
       entry = book.binary_search 'Billy'
       expect(entry).to be_nil
     end
